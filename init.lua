543f3ee9fefcad493e7f9923292402f4b6f561bb
kanopo <dmitri.ollari@protonmail.com>
2023-12-14 17:50:24 +0100

    base

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
 
-require("kanopo.options")
-require("kanopo.lazy")
 
 
--- lua/kanopo/plugins/commandline.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'VonHeikemen/fine-cmdline.nvim',
-    keys = {
-        { ":", "<cmd>FineCmdline<cr>", { noremap = true } },
-    },
-    dependencies = {
-        'MunifTanjim/nui.nvim',
-    },
-}
