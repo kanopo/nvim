d617b17b3d4dee2d9bc76219cbaf2b83949de897
kanopo <dmitri.ollari@protonmail.com>
2023-12-18 11:41:48 +0100

    lsp

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,47 @@
+return {
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"tsserver",
+				"lua_ls",
+				"pyright",
+			},
+			automatic_installation = true, -- not the same as ensure_installed
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				-- javascript
+				"prettier",
+				"eslint_d",
+
+				-- lua
+				"stylua",
+
+				-- python
+				"isort",
+				"black",
+				"pylint",
+			},
+		})
+	end,
+}
