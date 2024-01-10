76424279e672bf1e75e191866710470f8d59f5a0
kanopo <dmitri.ollari@protonmail.com>
2024-01-10 09:41:57 +0100

    lsp cmp line git commets nonels autopairs

---
+++ lua/user/nonels.lua
@@ -0,0 +1,23 @@
+local M = {
+  "jose-elias-alvarez/null-ls.nvim",
+}
+
+function M.config()
+  local null_ls = require "null-ls"
+
+  local formatting = null_ls.builtins.formatting
+
+  null_ls.setup {
+    sources = {
+      formatting.stylua,
+      formatting.prettier,
+      formatting.prettier.with {
+        extra_filetypes = { "toml" },
+      },
+      -- null_ls.builtins.diagnostics.eslint,
+      null_ls.builtins.completion.spell,
+    },
+  }
+end
+
+return M
