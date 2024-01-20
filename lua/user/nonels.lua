4b60898e669e76618106634820226b8bab65c0f6
kanopo <dmitri.ollari@protonmail.com>
2024-01-20 14:46:52 +0100

    fix typo nonels

--- lua/user/nonels.lua
+++ lua/user/nonels.lua
@@ -1,24 +1,25 @@
 local M = {
-  "jose-elias-alvarez/null-ls.nvim",
+    "jose-elias-alvarez/null-ls.nvim",
 }
 
 function M.config()
-  local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
-  local formatting = null_ls.builtins.formatting
-  local diagnostics = null_ls.builtins.diagnostics
+    local formatting = null_ls.builtins.formatting
+    local diagnostics = null_ls.builtins.diagnostics
 
-  null_ls.setup {
-    sources = {
-      -- TS
-      formatting.eslint_d,
-      diagnostics.eslint_d,
+    null_ls.setup {
+        sources = {
+            -- TS
+            formatting.eslint_d,
+            diagnostics.eslint_d,
 
 
-      -- python
-      formatting.black,
-      diagnostics.flake8,
-  }
+            -- python
+            formatting.black,
+            diagnostics.flake8,
+        }
+    }
 end
 
 return M
