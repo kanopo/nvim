e9dc74d9fc95e5d087278c7fc18860978bcab091
kanopo <dmitri.ollari@protonmail.com>
2024-03-17 11:38:20 +0100

    lang

--- lua/kanopo/keymaps.lua
+++ lua/kanopo/keymaps.lua
@@ -5,3 +5,5 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- stay in indent mode
 vim.keymap.set('v', '<', '<gv', { noremap = true, silent = true })
 vim.keymap.set('v', '>', '>gv', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
